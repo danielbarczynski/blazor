0d9ca38880ff828165516ca91bbe38909772bfec
Daniel PC <daniel.barczynski@onet.pl>
2022-11-21 22:56:54 +0100

    Add project files.

---
+++ BlazorApp1/Logic/Values.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorApp1.Logic
+{
+    public class Values
+    {
+        public int Rand1 { get; set; }
+        public int Rand2 { get; set; }
+ 
+
+        public Values()
+        {
+            Rand1 = Random.Shared.Next(1, 1000);
+            Rand2 = Random.Shared.Next(1, 1000);
+        }
+    
+    }
+}
